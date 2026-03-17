d22b5f501283d94f7e191e981dce49c85808a037
chaitu1712 <chaitanya.pandey.python@gmail.com>
2026-03-18 01:50:47 +0530

    feat: add cycle_a and cycle_b modules with interdependent functions to test cycle alerts

---
+++ test_cross_lang/cycle_a.py
@@ -0,0 +1,5 @@
+from test_cross_lang import cycle_b
+
+def do_something():
+    print("Doing something in A")
+    cycle_b.do_something_else()
---
+++ test_cross_lang/cycle_b.py
@@ -0,0 +1,5 @@
+from test_cross_lang import cycle_a
+
+def do_something_else():
+    print("Doing something in B")
+    cycle_a.do_something()
